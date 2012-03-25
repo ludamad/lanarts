7ae4972ef936f998d789f0a19f0b350523251e4c
putterson <putterson@gmail.com>
2012-03-24 20:38:16 -0400

    fixed clicking on second or more inventory item

--- src/data/sprite_data.cpp
+++ src/data/sprite_data.cpp
@@ -26,7 +26,7 @@ SpriteEntry game_sprite_data[] = {
 		SpriteEntry("ciribot", "res/monsters/ciribo.png"),
 		SpriteEntry("hell forged", "res/monsters/hellforged.png"),
 
-		SpriteEntry("mana potion", "res/items/misc/brilliant_blue.png"),
+		SpriteEntry("mana potion", "res/items/misc/brilliant_blue.png")
 };
 
 size_t game_sprite_n = sizeof(game_sprite_data)/sizeof(SpriteEntry);
