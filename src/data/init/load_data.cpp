85d6de47853fef7f91c4058ffdfba8b71ef397d3
Pat Goebel <putterson@gmail.com>
2012-03-26 22:09:22 -0400

    added weapon data loading

--- src/data/init/load_data.cpp
+++ src/data/init/load_data.cpp
@@ -71,3 +71,52 @@ void load_sprite_data(const char* filename){
 		game_sprite_data[i].init();
 	}
 }
+
+void load_weapon_data(const char* filename){
+	
+    fstream file(filename, fstream::in | fstream::binary);
+
+
+	YAML::Parser parser(file);
+	YAML::Node root;
+	
+	
+	string name, filen;
+	int issolid = 0;
+	
+	parser.GetNextDocument(root);
+	game_weapon_data.clear();
+	
+	const YAML::Node& node = root["Weapons"];
+
+	for(int i = 0; i < node.size(); i++){
+		issolid = 0;
+		const YAML::Node& n = node[i];
+		
+		int base_dmg, range, cooldown, proj_speed;
+		float mult_dmg;
+		
+		n["dmg_base"] >> base_dmg;
+		n["dmg_mult"] >> mult_dmg;
+		n["range"] >> range;
+		n["cooldown"] >> cooldown;
+		
+		WeaponType entry(
+				parse_cstr(n["weapon"]),
+				parse_defaulted(n , "projectile", 0),
+				base_dmg,
+				mult_dmg,
+				range,
+				cooldown,
+				parse_cstr(n["spr_weap"]),
+				parse_cstr(n["spr_attack"]),
+				parse_defaulted(n , "proj_speed", 0)
+		);
+		
+		game_weapon_data.push_back(entry);
+	}
+// 	for (int i = 0; i < game_sprite_data.size();i++){
+// 		game_sprite_data[i].init();
+// 	}
+}
+
