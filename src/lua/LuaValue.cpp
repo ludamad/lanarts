aa80b9020fc8fc159f6bedabb2133226a4f4cfd8
Adam Domurad <adomurad@redhat.com>
2012-05-16 17:16:48 -0400

    Added lua files

---
+++ src/lua/LuaValue.cpp
@@ -0,0 +1,18 @@
+/*
+ * LuaValue.cpp
+ *
+ *  Created on: May 16, 2012
+ *      Author: adomurad
+ */
+
+#include "LuaValue.h"
+
+LuaValue::LuaValue() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LuaValue::~LuaValue() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/lua/LuaValue.h
@@ -0,0 +1,17 @@
+/*
+ * LuaValue.h
+ *
+ *  Created on: May 16, 2012
+ *      Author: adomurad
+ */
+
+#ifndef LUAVALUE_H_
+#define LUAVALUE_H_
+
+class LuaValue {
+public:
+	LuaValue();
+	~LuaValue();
+};
+
+#endif /* LUAVALUE_H_ */
