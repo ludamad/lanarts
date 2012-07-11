af15e5558392a038181ec96d8ee2af66a4da81ba
adomurad <adomurad@linux-laptop.(none)>
2012-07-11 13:21:49 -0400

    Naive about what ccache does, removing from build scripts

--- make.sh
+++ make.sh
@@ -1,11 +1,6 @@
 mkdir -p ../lanarts_build
 cd ../lanarts_build
 
-# Ccache speeds up compilation, a LOT
-if [ -e /usr/lib/ccache ]; then 
-	export PATH=/usr/lib/ccache:$PATH
-fi
-
 cmake ../lanarts
 
 ## START TIMER CODE
@@ -13,7 +8,7 @@ T="$(date +%s%N)"
 ## START TIMER CODE
 
 ##MAKE
-make -j4
+VERBOSE=1 make -j4
 
 ## END TIMER CODE
 T="$(($(date +%s%N)-T))"
