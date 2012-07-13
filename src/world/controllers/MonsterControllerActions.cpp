e7e395c5feb2dfaf09f5343409afa3d4053a7627
Demur Rumed <gunkmute@gmail.com>
2012-07-13 15:48:12 +0000

    Revert "Dont move randomly in tight corridors"
    This reverts commit 6f36787014f2d8488c66f36a384bf381b666db50.

--- src/world/controllers/MonsterControllerActions.cpp
+++ src/world/controllers/MonsterControllerActions.cpp
@@ -40,8 +40,7 @@ static bool choose_random_direction(GameState* gs, EnemyInst* e, float& vx,
 		int nx = round(e->rx + vx * TILE_SIZE), ny = round(
 				e->ry + vy * TILE_SIZE);
 
-		//Use TILE_SIZE for checking solidity so that we don't jiggle in corridors
-		bool solid = gs->tile_radius_test(nx, ny, TILE_SIZE);
+		bool solid = gs->tile_radius_test(nx, ny, e->radius);
 		if (!solid) {
 			vx *= movespeed, vy *= movespeed;
 			return true;
@@ -79,8 +78,7 @@ bool potentially_randomize_movement(GameState* gs, EnemyInst* e) {
 		}
 		if (randomized) {
 			int nx = round(e->rx + er.vx), ny = round(e->ry + er.vy);
-			//Use TILE_SIZE for checking solidity so that we don't jiggle in corridors
-			bool solid = gs->tile_radius_test(nx, ny, TILE_SIZE);
+			bool solid = gs->tile_radius_test(nx, ny, e->radius);
 			if (!solid) {
 				e->vx = er.vx, e->vy = er.vy;
 			} else {
