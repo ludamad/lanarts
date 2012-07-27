a7d5718c51c9ddd52ea2f94e071afcc301ecbcb1
Adam Domurad <adomurad@redhat.com>
2012-07-27 17:06:16 -0400

    Berserking enemies

--- res/enemies/enemies.lua
+++ res/enemies/enemies.lua
@@ -10,8 +10,8 @@ end
 
 function enemy_berserker_step(enemy)
 	enemy.berserk_time = enemy.berserk_time - 1
-	if enemy.berserk_time <= 0 then	
-		enemy:add_effect("Berserk")
+	if enemy.berserk_time <= 0 then
+		enemy:add_effect("Berserk", 200)
 		enemy.berserk_time = 500
 	end
 end
