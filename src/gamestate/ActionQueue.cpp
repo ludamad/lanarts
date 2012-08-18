abae37f6c508004a2534793cdeff852acdba487c
Adam <domuradical@gmail.com>
2012-08-18 18:11:27 -0400

    Adding forgotten files

---
+++ src/gamestate/ActionQueue.cpp
@@ -0,0 +1,33 @@
+/*
+ * ActionQueue.h:
+ *  Provides ActionQueue class, which is an alias for a deque.
+ */
+
+#include "ActionQueue.h"
+
+void MultiframeActionQueue::extract_actions_for_frame(ActionQueue & actions,
+		int frame) {
+	actions.clear();
+	while (!queue.empty() && queue.front().frame <= frame) {
+		actions.push_back(queue.front());
+		queue.pop_front();
+	}
+}
+
+bool MultiframeActionQueue::has_actions_for_frame(int frame) {
+	for (int i = 0; i < frames_set.size() || frames_set[i] > frame; i++) {
+		if (frames_set[i] == frame) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void MultiframeActionQueue::queue_actions_for_frame(const ActionQueue & actions,
+		int frame) {
+	frames_set.push_back(frame);
+	for (int i = 0; i < actions.size(); i++) {
+		queue.push_back(actions[i]);
+	}
+}
+
