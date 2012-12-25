85f61163aa8677229434b507193ea91913a4a7a2
Adam Domurad <100397561@UOSL11-R9E3ETB.oncampus.local>
2012-12-25 12:14:03 -0500

    Balance

--- res/spells/spells.lua
+++ res/spells/spells.lua
@@ -9,3 +9,31 @@ function attack_all_seen(caster, _, _)
 	end
 
 end
+
+
+powerstrike_spell = {}
+
+function powerstrike_spell.action(caster, _, _) 
+    if caster:is_local_player() then 
+      show_message("You strike wildly in all directions!", {200,200,255}) 
+    elseif caster.name == "Your ally" then
+      show_message(caster.name .. " strikes wildly in all directions!", {200,200,255}) 
+    end
+    caster:add_effect("Charge", 8).callback = 
+      function (effect, caster)
+        local mons = level.monsters_list()
+        for num=1,#mons do
+          local mon = mons[num]
+          if distance({mon.x, mon.y}, {caster.x, caster.y}) < mon.target_radius + caster.target_radius + 35 then
+            caster:melee(mon)
+            local chance = math.max(25, 100 - (num-1) * 20) 
+            if rand_range(0, 100) > chance then -- decreasing chance of knockback
+                mon:add_effect("Thrown", 45 + 2 * caster.stats.level).angle = direction({caster.x, caster.y}, {mon.x, mon.y})
+                if caster:is_local_player() then 
+                  show_message("The target is thrown back!", {200,200,255}) 
+                end
+            end
+          end
+        end
+      end
+end
