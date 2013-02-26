d0c9e03ad3560c9be38ee4628dbabbf918ec2ec6
Adam Domurad <domuradical@gmail.com>
2013-02-26 17:32:07 -0500

    Start of class lua parsing impl

---
+++ res/classes/classes.lua
@@ -0,0 +1,92 @@
+data.class_create {
+    name = "Mage",
+    sprites = {"wizard", "wizard2"},
+    spells = {
+    },
+    start_stats = {
+        movespeed = 4,
+        hp = 85,
+        mp = 100,
+        hpregen = 0.033,
+        mpregen = 0.066,
+        strength = 3,
+        defence = 3,
+        willpower = 5,
+        magic = 5,
+        equipment = {
+          inventory = { item = "Mana Potion" }
+        }
+    },
+    gain_per_level = {
+        hp = 15,
+        mp = 20,
+        hpregen = 0.010,
+        mpregen = 0.007,
+        magic = 2,
+        strength = 1,
+        defence = 1,
+        willpower = 2
+    }
+}
+
+data.class_create {
+    name = "Fighter",
+    sprites = {"wizard", "wizard2"},
+    spells = {
+    },
+    start_stats = {
+        movespeed = 4,
+        hp = 85,
+        mp = 100,
+        hpregen = 0.033,
+        mpregen = 0.066,
+        strength = 3,
+        defence = 3,
+        willpower = 5,
+        magic = 5,
+        equipment = {
+          inventory = { item = "Mana Potion" }
+        }
+    },
+    gain_per_level = {
+        hp = 15,
+        mp = 20,
+        hpregen = 0.010,
+        mpregen = 0.007,
+        magic = 2,
+        strength = 1,
+        defence = 1,
+        willpower = 2
+    }
+}
+
+data.class_create {
+    name = "Archer",
+    sprites = {"wizard", "wizard2"},
+    spells = {
+    },
+    start_stats = {
+        movespeed = 4,
+        hp = 85,
+        mp = 100,
+        hpregen = 0.033,
+        mpregen = 0.066,
+        strength = 3,
+        defence = 3,
+        willpower = 5,
+        magic = 5,
+        equipment = {
+          inventory = { item = "Mana Potion" }
+        }
+    },
+    gain_per_level = {
+        hp = 15,
+        mp = 20,
+        hpregen = 0.010,
+        mpregen = 0.007,
+        magic = 2,
+        strength = 1,
+        defence = 1,
+        willpower = 2
+    }
+}
