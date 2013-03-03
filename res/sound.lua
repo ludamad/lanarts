250169b58dc488b2ad8fb962859ae65a11e77aac
Pat Goebel <putterson@gmail.com>
2013-03-02 20:38:02 -0500

    More sound updates

--- res/sound.lua
+++ res/sound.lua
@@ -2,9 +2,9 @@ require "utils"
 
 sounds = {}
 
-sounds.death_mage = sound_optional_load "res/sound/death_mage.wav" 
-sounds.death_fighter = sound_optional_load "res/sound/death_fighter.wav" 
-sounds.death_archer = sound_optional_load "res/sound/death_archer.wav" 
+sounds.death_mage = sound_optional_load "res/sound/death_player.wav" 
+sounds.death_fighter = sound_optional_load "res/sound/death_player.wav" 
+sounds.death_archer = sound_optional_load "res/sound/death_player.wav" 
 sounds.death_rat = sound_optional_load "res/sound/death_yelp.wav"
 sounds.death_bat = sound_optional_load "res/sound/death_yelp.wav"
 sounds.death_spider = sound_optional_load "res/sound/death_yelp.wav"
