5bcae2309b9abf8e742ac2a202a48632256ec38a
Adam Domurad <domuradical@gmail.com>
2013-03-03 22:33:53 -0500

    Ensure SDL net initializes for unit tests

--- src/lanarts/tests/tests.cpp
+++ src/lanarts/tests/tests.cpp
@@ -1,6 +1,10 @@
 #include <lcommon/unittest.h>
+#include <SDL_net.h>
 
 int main(int argc, char** argv) {
+	if (SDLNet_Init() < 0 ) {
+		printf("SDL Net failed to initialize!!\n");
+	}
 	run_unittests();
 	return 0;
 }
