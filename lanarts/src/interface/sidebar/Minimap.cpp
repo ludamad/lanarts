bd417aefac141d99dda33a33120559355cc5efc4
Adam Domurad <domuradical@gmail.com>
2013-08-12 23:23:44 -0400

    Much larger overworld map (fairly empty atm) and better minimap support for such

--- lanarts/src/interface/sidebar/Minimap.cpp
+++ lanarts/src/interface/sidebar/Minimap.cpp
@@ -49,19 +49,18 @@ static void draw_rect2d(char* buff, int w, int h, BBox bbox, const Colour& col) 
 }
 
 static void world2minimapbuffer(GameState* gs, char* buff,
-		const BBox& world_portion, int w, int h, int ptw, int pth) {
+		const BBox& shown, int w, int h, int ptw, int pth) {
 	GameTiles& tiles = gs->tiles();
 	GameView& view = gs->view();
 
-	bool minimap_reveal = gs->key_down_state(SDLK_z)
-			/*|| gs->key_down_state(SDLK_BACKQUOTE)*/;
+	bool minimap_reveal = gs->key_down_state(SDLK_z);
 
 	int stairs_down = res::tileid("stairs_down");
 	int stairs_up = res::tileid("stairs_up");
-	for (int y = 0; y < h; y++) {
+	for (int y = 0; y < shown.height(); y++) {
 		char* iter = buff + y * ptw * 4;
-		for (int x = 0; x < w; x++) {
-			Pos xy(x, y);
+		for (int x = 0; x < shown.width(); x++) {
+			Pos xy(x + shown.x1, y + shown.y1);
 			int tile = tiles.get(xy).tile;
 			int seen = tiles.is_seen(xy) || minimap_reveal;
 			if (seen) {
@@ -85,29 +84,33 @@ static void world2minimapbuffer(GameState* gs, char* buff,
 		if (is_enemy || is_feature) {
 			int ex = instance->x / TILE_SIZE;
 			int ey = instance->y / TILE_SIZE;
+			if (!shown.contains(ex, ey)) {
+				continue;
+			}
 
-                        bool seen = tiles.is_seen(Pos(ex, ey));
-                        if (is_enemy) {
-			    seen = seen && gs->object_visible_test(instance);
-                        } else if (is_feature) {
-                            bool was_seen = dynamic_cast<FeatureInst*>(instance)->has_been_seen();
-                            seen = seen && was_seen;
-                        }
-                        if (!seen && !minimap_reveal) {
-                            continue;
-                        }
+			bool seen = tiles.is_seen(Pos(ex, ey));
+			if (is_enemy) {
+				seen = seen && gs->object_visible_test(instance);
+			} else if (is_feature) {
+				bool was_seen =
+						dynamic_cast<FeatureInst*>(instance)->has_been_seen();
+				seen = seen && was_seen;
+			}
+			if (!seen && !minimap_reveal) {
+				continue;
+			}
 
 			if (is_enemy) {
-				set_colour(buff, ex, ey, ptw, Colour(255,0,0));
+				set_colour(buff, ex - shown.x1, ey - shown.y1, ptw, Colour(255, 0, 0));
 			} else if (is_feature) {
-				set_colour(buff, ex, ey, ptw, Colour(0,255,0));
-
+				set_colour(buff, ex - shown.x1, ey - shown.y1, ptw, Colour(0, 255, 0));
 			}
 		}
 	}
 }
 
-const int MINIMAP_SIZEMAX = 128;
+static const int MINIMAP_DIM_MAX = 128;
+static const Size MINIMAP_SIZE_MAX(MINIMAP_DIM_MAX, MINIMAP_DIM_MAX);
 
 Minimap::Minimap(const BBox& minimap_max_bounds) :
 		minimap_max_bounds(minimap_max_bounds), minimap_arr(NULL) {
@@ -137,6 +140,14 @@ static void init_minimap_buff(char* minimap_arr, int ptw, int pth) {
 	}
 }
 
+static BBox find_portion(Pos center, Size desired_size, Size world_size) {
+	Pos try_xy = center - Pos(desired_size.w/2, desired_size.h/2);
+	try_xy = Pos(std::min(try_xy.x, world_size.w - desired_size.w), std::min(try_xy.y, world_size.h - desired_size.h));
+	BBox world_bbox(Pos(), world_size);
+	BBox try_bbox = BBox(try_xy, desired_size).resized_within(world_bbox);
+	return BBox(try_bbox.left_top(), desired_size).resized_within(world_bbox);
+}
+
 // Draws a minimap from the contents of gs->tile_grid()
 void Minimap::draw(GameState* gs) {
 
@@ -145,34 +156,38 @@ void Minimap::draw(GameState* gs) {
 	BBox bbox = minimap_bounds(gs);
 
 	BBox view_region = view.tile_region_covered();
+	// Portion of world displayed on minimap
+	BBox world_portion = find_portion(view_region.center(), MINIMAP_SIZE_MAX, tiles.size());
+	printf("WORLD PORTION (%d %d) to (%d %d) size = (%d %d)\n", world_portion.x1, world_portion.y1, world_portion.x2, world_portion.y2, world_portion.width(), world_portion.height());
 
-	int ptw = power_of_two_round(MINIMAP_SIZEMAX), pth = power_of_two_round(
-			MINIMAP_SIZEMAX);
+	int ptw = power_of_two_round(MINIMAP_DIM_MAX), pth = power_of_two_round(
+			MINIMAP_DIM_MAX);
 	if (!minimap_arr) {
 		minimap_arr = new char[ptw * pth * 4];
 	}
 
 	init_minimap_buff(minimap_arr, ptw, pth);
 
-	world2minimapbuffer(gs, minimap_arr, BBox(), bbox.width(), bbox.height(), ptw, pth);
+	world2minimapbuffer(gs, minimap_arr, world_portion, bbox.width(), bbox.height(), ptw, pth);
 
+	BBox adjusted_view_region = view_region.translated(-world_portion.x1, -world_portion.y1);
 	PlayerInst* player = gs->local_player();
 	if (player) {
-		int arr_x = (player->x / TILE_SIZE), arr_y = (player->y / TILE_SIZE);
+		int arr_x = (player->x / TILE_SIZE) - world_portion.x1, arr_y = (player->y / TILE_SIZE) - world_portion.y1;
 //		fill_buff2d(minimap_arr, ptw, pth, arr_x - arr_x % 2, arr_y - arr_y % 2,
 //				Colour(255, 180, 99), 2, 2);
 		fill_buff2d(minimap_arr, ptw, pth, arr_x, arr_y, Colour(255, 180, 99), 1, 1);
-		draw_rect2d(minimap_arr, ptw, pth, view_region, Colour(255, 180, 99));
+		draw_rect2d(minimap_arr, ptw, pth, adjusted_view_region, Colour(255, 180, 99));
 	}
 	minimap_buff.from_bytes(Size(ptw, pth), minimap_arr);
 
-	Size size(128 / scale, 128 / scale);
-	BBox draw_region(view_region.center() - Pos(size.w / 2, size.h / 2), size);
-	draw_region = draw_region.resized_within(BBox(Pos(0,0), Size(128,128)));
+	Size size(MINIMAP_DIM_MAX / scale, MINIMAP_DIM_MAX / scale);
+	BBox draw_region(adjusted_view_region.center() - Pos(size.w / 2, size.h / 2), size);
+	draw_region = find_portion(adjusted_view_region.center(), size, world_portion.size());
 
 	// Prevent blurriness when scaling
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	minimap_buff.draw(ldraw::DrawOptions(draw_region).scale(SizeF(scale,scale)), bbox.left_top());
+	minimap_buff.draw(ldraw::DrawOptions(draw_region).scale(SizeF(scale,scale)), bbox.left_top() + Pos(25,25));
 	if (bbox.contains(gs->mouse_pos())) {
 		if (gs->mouse_left_click()) {
 			scale *= 2;
