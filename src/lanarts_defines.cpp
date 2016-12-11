eb9f8cca7fcadb4522bf4e4b6e4ad90dc8e906b5
Pat Goebel <putterson@gmail.com>
2016-12-11 01:36:21 -0500

    Use SDL_Quit instead of SDL_QuitSubSystem

--- src/lanarts_defines.cpp
+++ src/lanarts_defines.cpp
@@ -8,13 +8,6 @@
 
 void lanarts_system_quit() {
 	lanarts_net_quit();
-	SDL_QuitSubSystem(SDL_INIT_EVERYTHING ^ SDL_INIT_AUDIO);
-
 	lsound::stop_music();
-#ifndef __LINUX__
-	// Problem with WaitDone for AudioDevice callback using PulseAudio: http://pulseaudio.org/ticket/866
-	// We simply do not quit the audio subsystem on linux ... pity
-	lsound::deinit();
-	SDL_QuitSubSystem(SDL_INIT_AUDIO);
-#endif
+	SDL_Quit();
 }
