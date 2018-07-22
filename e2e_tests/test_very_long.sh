eed17b582b729c39078a03f1a2e7188c3d9ed808
adomurad <adomurad@localhost.localdomain>
2018-07-22 16:44:18 -0400

    Add test_very_long.sh

---
+++ e2e_tests/test_very_long.sh
@@ -0,0 +1,7 @@
+set -e
+
+temp_dir=`mktemp -d`
+trap "{ rm -rf \"temp_dir\"; }" EXIT # clean up temp dir on exit
+
+echo "Testing 1000000 steps"
+./run.sh -g --headless engine.StartBotTests --seed=112312001 --steps 1000000 
